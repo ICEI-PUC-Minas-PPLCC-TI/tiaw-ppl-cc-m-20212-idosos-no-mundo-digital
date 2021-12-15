21a8e2e0a9f119f452e38b6ced0ad43306ff5dc4
Bernard Paes <89420728+BernardPaes@users.noreply.github.com>
2021-12-14 22:32:42 -0300

    Código Site

---
+++ Codigo/exibicao.js
@@ -0,0 +1,96 @@
+var db_artigos_inicial = {
+    "data": [
+        {
+            "id": 1,
+            "titulo": "Especialistas defendem uso de tecnologia em favor da qualidade de vida dos idosos",
+            "autor": "Redação",
+            "categoria": "Notícias",
+            "texto": "A Subcomissão Permanente de Proteção e Defesa da Pessoa Idosa discutiu nesta quinta-feira (26) a gerontecnologia, área de conhecimento multidisciplinar que trata do desenvolvimento de estudos e pesquisas para contribuir para a longevidade e qualidade de vida da população idosa. Por meio de inovações tecnológicas, a gerontecnologia permite mais independência e autonomia para os idosos. A reunião foi uma iniciativa do senador Eduardo Gomes (MDB-TO). Uma das convidadas para a audiência pública, Carla Santana, presidente da Sociedade Brasileira de Gerontecnologia, afirmou que o Brasil passa por uma mudança demográfica significativa, com crescente número de idosos, e com isso há demandas específicas para essa parcela da população.— É um convite a gente pensar que o processo de envelhecimento é real, é concreto, e vai impactar todos os mercados. É importante pensar que o impacto é na indústria, no setor de hospedagem, indústria alimentícia, seguros, robótica, saúde. No exterior todos esses mercados já estão se adaptando ao envelhecimento humano e acho que, no Brasil, ele é um nicho importante para ser ampliado no âmbito das suas oportunidades de desenvolvimento de produtos, serviços e conhecimento de uma forma mais ampla — defendeu. Já o secretário Nacional de Promoção e Defesa da Pessoa Idosa do Ministério da Mulher, Família e dos Direitos Humanos, Antonio Costa, destacou o Programa Viver, do governo federal, que tem por objetivo potencializar as habilidades necessárias da pessoa idosa para um envelhecimento ativo e saudável por meio de quatro campos de ação: tecnologia, educação, saúde e mobilidade física. — A nossa secretaria faz a doação de uma rede de computadores e nós trabalhamos inicialmente em quatro eixos, que é o eixo da inclusão digital, a introdução ao celular, a introdução ao smartphone, aos aplicativos que são necessários para que esses idosos tenham oportunidade de serem inseridos no mundo digital — afirmou Costa.  Tecnologias assistivas: O senador Eduardo Gomes aproveitou a presença do representante do Ministério para pedir um roteiro dos programas do governo que tratem de tecnologias assistivas para que possam receber recursos de emendas parlamentares na proposta de Lei Orçamentária de 2020. A audiência pública faz parte das atividades propostas pelo plano de trabalho da subcomissão, criada em maio deste ano para promoção e defesa dos direitos e da inclusão da pessoa idosa, além de fiscalizar e acompanhar programas governamentais."
+        },
+        {
+            "id": 2,
+            "titulo": "Tecnologia não é pensada para idosos?",
+            "autor": "Carina GOnçalves",
+            "categoria": "Notícias",
+            "texto": "O celular, a internet, o computador são coisas de gente jovem? As mudanças de hábitos potencializadas pela pandemia de covid-19 só provam ainda mais que essa crença está ultrapassada. Porém, a jornada de aprendizado para muitos idosos pode ser desafiadora. Dificuldades auditivas, visuais e/ou motoras que alguns desenvolvem com o passar dos anos refletem nesse processo, segundo especialistas ouvidos por Tilt. De modo geral, os idosos costumam ter três desafios para ingressar no mundo digital: Quebrar a ideia de que a tecnologia é coisa de gente mais nova Aprender a usá-las, uma vez que muitos precisam de auxílio de alguém para ensinar Desconhecer recursos de acessibilidade que facilitam o uso no dia a dia (que você poderá conferir exemplos mais a baixo). Marilda Delunardo, 72, queria distância do celular por achar que era coisa de gente jovem até a pandemia acontecer. Isso mudou quando ela viu sua independência ser reduzida ao espaço domiciliar e as reuniões do centro espírita que frequentava migrarem para o Zoom (serviço videochamadas pela internet). Com ajuda da neta Lígia, Delunardo aprendeu a usar o celular e aplicativos para se manter conectada com o mundo lá fora. Hoje, ela é considerada pela neta uma avó super tecnológica, que anda pela casa com o smartphone, tablet e computador. 'No WhatsApp é uma letra maior e o resto é tranquilo', afirma Delunardo, diagnosticada com catarata, e que possui dificuldades de locomoção. 'Foi uma opção na pandemia. Ou eu 'ficava com 72 anos' ou 'com 22'. Então preferi fingir que tenho 22 para poder viver [usando a tecnologia].Aprendizado precisa ser adaptativo Assim como Delunardo, pensar que a tecnologia está ligada à juventude é comum entre os idosos, de acordo com Luli Radfahrer, professor de comunicação digital da ECA (Escola de Comunicações e Artes) da USP (Universidade de São Paulo). Os mais velhos não viveram uma parte da curva da tecnologia. O processo de aprendizado para eles tem que ser adaptativo, não se pode ensinar informática da mesma maneira como seria ensinado a um jovem. Primeiro, precisa adaptar o que já existe para ele [para o idoso], um outro contexto e outras necessidades que precisamos compreendê-las para fazermos alguma coisa, explica Radfahrer, especialista nos impactos  da internet na sociedade. De repente, você consegue associar o conhecimento de uma vida de experiências [que os idosos possuem] com conhecimentos novos, afirma o professor. Envelhecimento conectado O uso de tecnologias por pessoas mais velhas se encaixa dentro do conceito de envelhecimento conectado, segundo Luciana Corrêa, fundadora da LSC Educação e Projetos, que promove ações de impacto educativo para idosos. Corrêa explica que muitos avós passaram a ter interesse em dispositivos ao ver seus netos usando com tanta facilidade. O que muda entre um consumidor jovem e um consumidor maduro é como essas funcionalidades serão utilizadas. Mas o desejo de consumo é o mesmo. No mercado de celulares, existem algumas (poucas) opções pensadas exclusivamente para os consumidores idosos, como telefones vendidos de fábrica já com ícones, letras e números maiores. Eles costumam possuir aplicativos básicos de comunicação já instalados, como WhatsApp e Facebook. Alguns possuem ainda o recurso de segurança SOS integrado, no qual, em caso de necessidade, o aparelho emite um alerta de emergência (por mensagem ou ligação). Outros modelos investem em teclados físicos no lugar das telas sensíveis ao toque. No resto, eles se conectam normalmente à internet normalmente e permitem o uso de apps de mensagens. Ortelina Ferreira, 97, usa um aparelho auditivo. Para ouvir ligações e áudios do WhatsApp, a sua família conseguiu configurar o dispositivo junto ao smartphone. Assim, ela pode ouvir os sons recebidos diretamente do aparelho."
+        {
+            "id": 3,
+            "titulo": "App visa facilitar acesso ao crédito consignado com linguagem adaptada",
+            "autor": "Patricia Soares",
+            "categoria": "Notícias",
+            "texto": "Ao perceber que existia pouca tecnologia pensada para a terceira idade, a publicitária Patrícia Soares, 36, ficou inquieta. Pensando em facilitar a vida deste público e contribuir com a inclusão digital e social, ela e o sócio, Diego Andrade, que já trabalhavam juntos no mercado financeiro, montaram a Prestho, fintech de Minas Gerais voltada a facilitar o acesso ao crédito. Lançada em 2019, a plataforma, nos últimos meses, cresceu 331,6% em volume de acessos.Com experiência de quase 20 anos com crédito consignado, Patrícia lembra que o principal desafio da ideia era eliminar a burocracia e a papelada do processo de obtenção de crédito. Queríamos que o público fosse inserido no mundo da inovação, além de entregar a eles a autonomia para simular e contratar um empréstimo a qualquer hora e dia, sem sair de casa. Seja de madrugada ou em um domingo, afirma. Antes da plataforma ir ao ar, durante 12 meses, foi realizado um estudo sobre o comportamento e necessidades das pessoas idosas. No desenvolvimento, analisamos que tipo de aparelho o público mais usava. A maioria tem aparelhos Android, com pouco espaço de armazenamento e, desta forma, o aplicativo tinha que ser leve, conta.Ao desenvolver um aplicativo voltado para os mais velhos, eles previram as eventuais dificuldades. O jovem já está inserido neste mundo digital e navega com facilidade em um aplicativo que foi desenvolvido pensando nas necessidades específicas dos seniores digitais. Já uma pessoa de 65 anos, que tem dificuldade de se relacionar com a tecnologia, não vai ter uma boa experiência em um aplicativo que for focado para jovens, exemplifica. A solução, então, foi desenvolver uma plataforma com linguagem adaptada, tamanhos de fontes e botões diferentes, cores, vídeo explicativo durante a jornada de contratação, informações claras e simples. Para a publicitária, é preciso, cada vez mais, dar atenção ao nicho 60 . Os estudos indicam que, em 2030, o número de idosos ultrapassará o número de crianças. É um público esquecido pelo mercado, destaca. Como tendências de mercado, Patrícia acredita que os idosos ficarão ainda mais ativos nos serviços digitais e de e-commerce. Tivemos simulações de crédito por usuários acima de 60 anos entre 1h e 5h da manhã. Este comportamento mostra que os idosos estão cada vez mais conectados, acredita."
+        },
+        {
+            "id": 4,
+            "titulo": "Neo Química quer acelerar soluções que foquem nos mais maduros",
+            "autor": "Rafael Ucha",
+            "categoria": "Notícias",
+            "texto": "Para selecionar ideias e contribuir com a expansão de negócios focados no público 60 , a Neo Química, marca do setor farmacêutico brasileiro, através do seu programa Neo Acelera, está promovendo a segunda edição do projeto Longevidade Ativa. Com inscrições já encerradas, o programa apoia projetos sociais que tenham potencial para ganhar escala. Neste ano, o foco são startups e negócios que promovam inovações reais para os principais desafios enfrentados pelo consumidor maduro.São nove grandes temas que contemplam a iniciativa: aprendizagem ao longo da vida; ocupação e vida profissional; planejamento financeiro e saúde financeira; mobilidade e autonomia; movimentação e segurança; gestão integrada da saúde; cuidado e acompanhamento pessoal; mente ativa e saudável e cuidados com o fim da vida.Um dos desafios ao se desenvolver uma resposta ampla para o envelhecimento da população é olharmos para soluções segmentadas e sustentáveis no longo prazo. Uma parcela da população ainda sente dificuldade em encontrar produtos e serviços que enderecem suas necessidades especificas, afirma a gerente executiva da Neo Química, Natália Niro.Para ela, os empreendedores que desejam trabalhar com esse tema devem, em primeiro lugar, entender a fundo o perfil de comportamento dessas pessoas. Natália destaca que elas respondem pela terceira maior atividade econômica do mundo, movimentando mais de US$ 15 trilhões por ano globalmente, e a busca por produtos e soluções segmentadas e personalizadas é cada vez maior, ao passo que a oferta ainda é desequilibrada.Necessidades não atendidas ao longo da vida, limitações de acesso e exclusão continuada sofrem um efeito acumulativo que se amplifica na velhice. Então, precisamos olhar para soluções que resolvam essas lacunas: qualidade de vida, mente ativa, mobilidade e segurança, aplicativos e programas de reinserção social e planejamento financeiro, por exemplo. O Neo Acelera tem parceria com a investidora Yunus Negócios Sociais Brasil. Para Rafael Ucha, consultor em aceleração e inovação social da Yunus, o principal desafio de desenvolver negócios para o público 60 é desbravar um mercado em que poucos estiveram antes.Segundo ele, esses indivíduos estiveram, até então, invisibilizados e marginalizados da sociedade em diversos aspectos. Isso resulta em pouco conhecimento prévio sobre essas pessoas como consumidores. É necessário um trabalho para quebrar preconceitos e transformar percepções enviesadas e antiquadas que a sociedade ainda cultiva, destaca.Desenvolver empatia, dar voz e envolver a população longeva no processo cocriativo dessas soluções são peças-chave. Um forte indício da importância disso é que muitos dos negócios bem sucedidos nesse segmento são capitaneadas por pessoas 60 , que se cansaram e resolveram, por conta própria, criar soluções para problemas que eles mesmos enfrentavam, afirma Rafael. Os negócios que estiverem concorrendo no programa Neo Acelera serão conhecidos em novembro deste ano."
+        },
+        {
+            "id": 5,
+            "titulo": "Tecnologia na terceira idade em tempos de pandemia. Quais os benefícios?",
+            "autor": "Gian Franco Balto",
+            "categoria": "Notícias",
+            "texto": "Nunca nos vimos tão conectados como nos dias atuais. A internet tem tido um crescimento exponencial nos últimos anos, e assim muito novos adeptos. Os internautas passaram a dinamizar o seu dia a dia com o auxílio da ferramenta digital, seja para trabalhar, fazer compras e entreter. Em tempos de pandemia, a internet passou a ter um papel primordial para muitas pessoas, auxiliando para o isolamento social. As chamadas de vídeo e envio de mensagens vem mantendo a proximidade e a interação entre muitos amigos e familiares. Mesmo com tamanha mudança de comportamento, muitos ainda pensam que a tecnologia ainda pode ser um obstáculo para os idosos. Será mesmo?. Uma pesquisa recente realizada pela CNDL (Confederação Nacional de Dirigentes Lojistas) e pelo SPC Brasil, aponta que o número de idosos com mais de 60 anos com acesso a internet, subiu de 68 para 97 por cento nos últimos três anos no país. O meio de acesso mais queridinho dos nossos idosos para acesso a grande rede, continua sendo os smartphones com 84 por cento, seguido dos notebooks e computadores desktops. A importância deste público para o mercado tem sido foco de análise pelas grandes empresas, visto que boa parte dos idosos possui bom poder de compra, além da vida financeira estável, o que desperta a atenção do mercado. Atualmente, há aparelhos desenvolvidos para atender e facilitar o uso por parte dos idosos (entre smartphones, tablets e notebooks), além de cursos especializados voltados para o aprendizado no uso dessas tecnologias. Observamos então, que diante da inclusão digital direcionada aos idosos, as benesses surgem na mesma velocidade desse boom tecnológico. Dentre os quais, o acesso a bancos e soluções financeiras evitando filas e deslocamentos para agências; Os serviços de entrega que proporcionam uma infinidade de opções de compra nos mais diversos estabelecimentos, tais como farmácias, mercados e restaurantes; Além do auxílio à saúde com ferramentas que atuam no monitoramento da saúde dos idosos. Não obstante, podemos enaltecer os mecanismos de inclusão social e comunicação, onde aplicativos mensageiros e demais ferramentas promovem interação e reduzem a sensação de isolamento aumentando a auto estima dos idosos; Nessa casta, observamos que o entretenimento abre um leque de opções para os idosos, como ver e ouvir músicas e shows musicais, acompanhar e realizar apresentações de conteúdos de interesse pessoal, além do uso de redes sociais como mecanismo de interação entre seus amigos e familiares, trazendo distração e diversão e mantendo assim a mente ativa e suas capacidades cognitivas. O aprendizado nesse sentido possibilita novas vivências, descobertas e experiências, que podem contribuir sobremaneira para a qualidade de vida e saúde mental. Mas como podemos colaborar nesse sentido? Bom, por vezes nos pegamos com alguma dificuldade ou outra no uso de alguns aplicativos e precisamos recorrer ao auxílio de especialistas que detém um conhecimento maior da situação, através de chats, centrais telefônicas, entre outros. No caso dos idosos, é de grande auxílio compartilharmos o nosso conhecimento com eles, contribuindo para o uso dessas ferramentas, tirando dúvidas, apontando os riscos, e mostrando pacientemente como funciona cada ferramenta para promover o seu uso seguro. No meio tecnológico, sabemos que há muitos oportunistas que se aproveitam dos descuidos para aplicar golpes e tirar vantagens financeiras e nosso comportamento como amigos ou parentes desses idosos, é colaborar para que eles possam desfrutar da tecnologia de forma adequada e sem intempéries. Torna-se uma tarefa nobre e singela contribuir para que os nossos vovôs e vovós possam se beneficiar da inclusão digital, não apenas para a saúde mental dos mesmos, mas para trazer mais autonomia e independência para o seu dia a dia. Aqueles poucos minutos da nossa vida para dar atenção a quem sempre nos deu, pode fazer bem não apenas para nós, mas para uma infinidade de pessoas que procuram viver melhor."
+        }
+    ]
+}
+
+// Caso os dados já estejam no Local Storage, caso contrário, carrega os dados iniciais
+var db = JSON.parse(localStorage.getItem('db_artigos'));
+if (!db) {
+    db = db_artigos_inicial
+};
+
+// Exibe mensagem em um elemento de ID msg
+function displayMessage(msg) {
+    $('#msg').html('<div class="alert alert-warning">' + msg + '</div>');
+}
+
+function insertArtigo(artigo) {
+    // Calcula novo Id a partir do último código existente no array (PODE GERAR ERRO SE A BASE ESTIVER VAZIA)
+    let novoId = 1;
+    if (db.data.length != 0) 
+      novoId = db.data[db.data.length - 1].id + 1;
+    let novoArtigo = {
+        "id": novoId,
+        "titulo": artigo.titulo,
+        "texto": artigo.texto,
+        "autor" : artigo.autor,
+        "categoria": artigo.categoria
+    };
+
+    // Insere o novo objeto no array
+    db.data.push(novoArtigo);
+    displayMessage("Artigo inserido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_artigos', JSON.stringify(db));
+}
+
+function updateArtigo(id, artigo) {
+    // Localiza o indice do objeto a ser alterado no array a partir do seu ID
+    let index = db.data.map(obj => obj.id).indexOf(id);
+
+    // Altera os dados do objeto no array
+    db.data[index].titulo = artigo.titulo,
+    db.data[index].texto = artigo.texto,
+    db.data[index].autor = artigo.autor,
+    db.data[index].categoria = artigo.categoria
+
+    displayMessage("Artigo alterado com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_artigos', JSON.stringify(db));
+}
+
+function deleteArtigo(id) {    
+    // Filtra o array removendo o elemento com o id passado
+    db.data = db.data.filter(function (element) { return element.id != id });
+
+    displayMessage("Artigo removido com sucesso");
+
+    // Atualiza os dados no Local Storage
+    localStorage.setItem('db_artigos', JSON.stringify(db));
+}
